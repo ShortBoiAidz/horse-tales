9b84e68db1b09b1ea81485ee98fc36c28f437b2d
Aiden Thorpe <surzies@gmail.com>
2026-09-23 13:00:40 +1000

    feat: update map state management to include inner cave transition

--- data/states/mapstates.py
+++ data/states/mapstates.py
@@ -3,17 +3,24 @@ from data.components import player
 
 playerObject = player.Player()
 
-mapState = "caveEntrance"
+MAPSTATE = "caveEntrance"
+
 class currentMap:
   def __init__(self):
     self.img = pg.image.load("assets/textures/mapsheets/cave_entrance.png")
     self.img = pg.transform.scale(self.img, (800, 600))
+
     self.map = caveEntranceMap()
+
     self.rect = self.img.get_rect()
 
   def update(self, screen):
-    match mapState:
+    match MAPSTATE:
       case "caveEntrance":
+        self.map.drawMap(screen)
+        self.map.update()
+      case "innerCave":
+        self.map = innerCaveMap()
         self.map.drawMap(screen)
         self.map.update()
 
@@ -31,3 +38,14 @@ class caveEntranceMap:
   def update(self):
     if self.entranceRect.colliderect(playerObject.rect):
       print("COLLIDE")
+      MAPSTATE = "innerCave"
+
+class innerCaveMap:
+  def __init__(self):
+    pass
+
+  def drawMap(self, screen):
+    pass
+
+  def update(self):
+    pass
