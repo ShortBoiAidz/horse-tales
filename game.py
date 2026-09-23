418fd8bc481f669821a793b62273da64cc39fad9
Aiden Thorpe <surzies@gmail.com>
2026-09-23 13:15:19 +1000

    refactor: remove unused caveEntranceMap variable from init_pygame function

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ GAME_NAME = "Horse Tale"
 DEBUG = True
 
 def init_pygame():
-	global clock, carleigheSmirk, danielPrank, playerObject, currentMap, keys, caveEntranceMap
+	global clock, carleigheSmirk, danielPrank, playerObject, currentMap, keys
 
 	screen = pg.display.set_mode((WIDTH, HEIGHT))
 	clock = pg.time.Clock()
@@ -23,7 +23,6 @@ def init_pygame():
 
 	# Other objects
 	currentMap = mapstates.currentMap()
-	caveEntranceMap = mapstates.caveEntranceMap()
 
 	# Sprite groups
 	movingSprites = pg.sprite.Group()
